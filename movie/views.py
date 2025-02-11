f7587f9daf1fdefa2d347daeb29727f902cc7803
Jesús David Gómez Ávila <jdgomeza3@eafit.edu.co>
2025-02-10 22:18:58 -0500

    nuevo archivo about.html

--- movie/views.py
+++ movie/views.py
@@ -9,4 +9,4 @@ def home(request):
     return render(request, 'home.html', {'name': 'Jesús Gómez'})
 
 def about(request):
-    return HttpResponse('<h1>Welcome to about Page<h1>')
+    return render(request, 'about.html')
